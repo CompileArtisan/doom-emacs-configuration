c002e598a4ccd4d8087a9fae540e277c81d152c8
CompileArtisan <praanesh.b.nair@gmail.com>
2025-03-17 12:47:09 +0530

    Added the rss.xml file for the verge too

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
  '(elfeed-feeds
-   '("https://compileartisan.pages.dev/rss.xml" "https://adithyanair.com/rss.xml"))
+   '("http://theverge.com/rss/index.xml" "https://compileartisan.pages.dev/rss.xml" "https://adithyanair.com/rss.xml"))
  '(org-agenda-files
    '("~/OneDrive/Study Material/Semester-4/introduction to design analysis of algorithms DAA/notes/dsa1.org"))
  '(org-safe-remote-resources
