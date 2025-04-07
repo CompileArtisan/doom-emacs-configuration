89961ee1b42ee283be5450975a2be0b7ac614b9f
CompileArtisan <praanesh.b.nair@gmail.com>
2025-04-07 11:54:22 +0530

    changed themes

--- custom.el
+++ custom.el
@@ -3,31 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(custom-safe-themes
-   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
+   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
  '(elfeed-feeds
    '("https://www.team-bhp.com/forum/external.php?type=rss2" "http://theverge.com/rss/index.xml" "https://compileartisan.pages.dev/rss.xml" "https://adithyanair.com/rss.xml"))
  '(org-agenda-files
    '("~/OneDrive/Study Material/Semester-4/introduction to design analysis of algorithms DAA/notes/dsa1.org"))
- '(org-safe-remote-resources
-   '("\\`file:///home/praaneshnair/OneDrive/Study Material/Semester-4/introduction to design analysis of algorithms DAA/notes/my-notes-on-algorithms\\.org\\'"))
- '(package-selected-packages
-   '(citeproc-org elfeed org-superstar impatient-mode grip-mode))
- '(safe-local-variable-values
-   '((eval add-to-list 'org-export-before-processing-hook
-      (lambda
-        (be)
-        (org-babel-tangle)))
-     (eval setq org-latex-pdf-process
-      (list "latexmk -pdf %f"))
-     (eval require 'doi-utils)
-     (eval require 'org-ref)
-     (eval ox-extras-activate
-      '(ignore-headlines))
-     (eval require 'ox-extra)
-     (eval add-to-list 'load-path ".")))
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
  '(org-html-head-extra "")
  '(org-latex-classes
    '(("IEEEtran" "\\documentclass[conference]{IEEEtran}"
@@ -55,7 +38,22 @@
       ("\\subsection{%s}" . "\\subsection*{%s}")
       ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
  '(org-latex-image-default-width "0.3\\linewidth")
- '(package-selected-packages '(org-ref)))
+ '(org-safe-remote-resources
+   '("\\`file:///home/praaneshnair/OneDrive/Study Material/Semester-4/introduction to design analysis of algorithms DAA/notes/my-notes-on-algorithms\\.org\\'"))
+ '(package-selected-packages '(org-ref))
+ '(safe-local-variable-values
+   '((eval add-to-list 'org-export-before-processing-hook
+      (lambda
+        (be)
+        (org-babel-tangle)))
+     (eval setq org-latex-pdf-process
+      (list "latexmk -pdf %f"))
+     (eval require 'doi-utils)
+     (eval require 'org-ref)
+     (eval ox-extras-activate
+      '(ignore-headlines))
+     (eval require 'ox-extra)
+     (eval add-to-list 'load-path "."))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
