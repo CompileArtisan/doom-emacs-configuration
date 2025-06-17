6afde102b3acf406080ba8a7b513f015effe29e3
CompileArtisan <praanesh.b.nair@gmail.com>
2025-06-17 18:41:40 +0530

    hehe

--- config.el
+++ config.el
@@ -14,93 +14,93 @@
 (setq org-directory "~/org/")
 (setq copilot-mode nil)
 (use-package! minimap
- :config
- (setq minimap-window-location 'right
-       minimap-width-fraction 0.09
-       minimap-minimum-width 15
-       minimap-hide-cursor t
-       minimap-hide-fringes t)
- (custom-set-faces!
-   '(minimap-active-region-background
-     :background "#000000")))
+  :config
+  (setq minimap-window-location 'right
+        minimap-width-fraction 0.09
+        minimap-minimum-width 15
+        minimap-hide-cursor t
+        minimap-hide-fringes t)
+  (custom-set-faces!
+    '(minimap-active-region-background
+      :background "#000000")))
 (remove-hook 'server-after-make-frame-hook #'doom-display-benchmark-h)
 
 (use-package! web-mode
- :mode "\\.html?\\'"
- :config
- (setq web-mode-markup-indent-offset 2)
- (setq web-mode-css-indent-offset 2)
- (setq web-mode-code-indent-offset 2)
- (setq web-mode-enable-auto-pairing t)
- (setq web-mode-enable-auto-closing t))
+  :mode "\\.html?\\'"
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-closing t))
 
 (add-to-list 'auto-mode-alist '("\\.astro\\'" . web-mode))
 (after! web-mode
- (setq web-mode-markup-indent-offset 2)
- (setq web-mode-code-indent-offset 2)
- (setq web-mode-css-indent-offset 2))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2))
 
 (use-package pdf-tools
- :defer t
- :commands (pdf-loader-install)
- :mode "\\.pdf\\'"
- :bind (:map pdf-view-mode-map
-             ("j" . pdf-view-next-line-or-next-page)
-             ("k" . pdf-view-previous-line-or-previous-page)
-             ("C-+" . pdf-view-enlarge)
-             ("C--" . pdf-view-shrink))
- :init (pdf-loader-install)
- :config
- (add-to-list 'revert-without-query ".pdf"))
+  :defer t
+  :commands (pdf-loader-install)
+  :mode "\\.pdf\\'"
+  :bind (:map pdf-view-mode-map
+              ("j" . pdf-view-next-line-or-next-page)
+              ("k" . pdf-view-previous-line-or-previous-page)
+              ("C-+" . pdf-view-enlarge)
+              ("C--" . pdf-view-shrink))
+  :init (pdf-loader-install)
+  :config
+  (add-to-list 'revert-without-query ".pdf"))
 
 (add-hook 'pdf-view-mode-hook
-         (lambda ()
-           (display-line-numbers-mode -1)))
+          (lambda ()
+            (display-line-numbers-mode -1)))
 
 (setq confirm-kill-emacs nil)
 
 
 (after! tex
- (setq TeX-auto-save t)
- (setq TeX-parse-self t)
- (setq-default TeX-master t)
- (setq TeX-parse-all-errors t)
- (setq TeX-command-default "LatexMk"))
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master t)
+  (setq TeX-parse-all-errors t)
+  (setq TeX-command-default "LatexMk"))
 
 (use-package! latex-preview-pane
- :after latex
- :commands latex-preview-pane-mode
- :init
- (setq latex-preview-pane-multifile-mode 'auctex)
- (setq latex-preview-pane-update-delay 0.1)
- :config
- (setq latex-preview-pane-pdf-view-command "evince")
- (when (and (eq system-type 'gnu/linux)
-            (string-match "Microsoft" (shell-command-to-string "uname -a")))
-   (setq latex-preview-pane-path-translation-mappings
-         '(("/mnt/c/" . "C:/"))))
- (latex-preview-pane-enable))
+  :after latex
+  :commands latex-preview-pane-mode
+  :init
+  (setq latex-preview-pane-multifile-mode 'auctex)
+  (setq latex-preview-pane-update-delay 0.1)
+  :config
+  (setq latex-preview-pane-pdf-view-command "evince")
+  (when (and (eq system-type 'gnu/linux)
+             (string-match "Microsoft" (shell-command-to-string "uname -a")))
+    (setq latex-preview-pane-path-translation-mappings
+          '(("/mnt/c/" . "C:/"))))
+  (latex-preview-pane-enable))
 
 (after! org 
- (setq org-babel-default-header-args:java '((:dir . nil) (:results . "value"))) 
- (org-babel-do-load-languages 'org-babel-load-languages '((java . t))))
+  (setq org-babel-default-header-args:java '((:dir . nil) (:results . "value")))
+  (org-babel-do-load-languages 'org-babel-load-languages '((java . t))))
 
 (add-hook! 'latex-mode-hook
- (lambda ()
-   (unless TeX-master
-     (setq TeX-master (buffer-file-name)))
-   (auto-save-mode +1)
-   (setq auto-save-timeout 1)
-   (setq auto-save-interval 1)))
+  (lambda ()
+    (unless TeX-master
+      (setq TeX-master (buffer-file-name)))
+    (auto-save-mode +1)
+    (setq auto-save-timeout 1)
+    (setq auto-save-interval 1)))
 
 (defun force-latex-preview-update ()
- (interactive)
- (when (bound-and-true-p latex-preview-pane-mode)
-   (latex-preview-pane-update)))
+  (interactive)
+  (when (bound-and-true-p latex-preview-pane-mode)
+    (latex-preview-pane-update)))
 
 (map! :map latex-mode-map
-     :localleader
-     "p" #'force-latex-preview-update)
+      :localleader
+      "p" #'force-latex-preview-update)
 
 (add-hook 'org-mode-hook
           (lambda ()
@@ -108,26 +108,26 @@
 
 
 (map!
-:leader
-"ke" #'save-buffers-kill-terminal)
+ :leader
+ "ke" #'save-buffers-kill-terminal)
 
 (after! eyebrowse
- (centaur-tabs-headline-match))
+  (centaur-tabs-headline-match))
 
 (after! treemacs
- (setq treemacs-follow-current-file t)
- (setq treemacs-recenter-after-file-follow t)
- (setq treemacs-follow-after-init t)
- (setq treemacs-project-follow-cleanup t)
- (setq treemacs-show-hidden-files t))
+  (setq treemacs-follow-current-file t)
+  (setq treemacs-recenter-after-file-follow t)
+  (setq treemacs-follow-after-init t)
+  (setq treemacs-project-follow-cleanup t)
+  (setq treemacs-show-hidden-files t))
 
 (after! org 
- (set-face-attribute 'org-level-1 nil :height 1.1 :weight 'normal)
- (set-face-attribute 'org-level-2 nil :height 1.1 :weight 'normal)
- (set-face-attribute 'org-level-3 nil :height 1.1 :weight 'normal)
- (set-face-attribute 'org-document-title nil :height 1.5 :weight 'bold))
+  (set-face-attribute 'org-level-1 nil :height 1.1 :weight 'normal)
+  (set-face-attribute 'org-level-2 nil :height 1.1 :weight 'normal)
+  (set-face-attribute 'org-level-3 nil :height 1.1 :weight 'normal)
+  (set-face-attribute 'org-document-title nil :height 1.5 :weight 'bold))
 
-; Open Elfeed with the keybinding `SPC a e`
+                                        ; Open Elfeed with the keybinding `SPC a e`
 (use-package! elfeed)
 (eval-after-load 'elfeed
   '(map! :leader
@@ -144,8 +144,8 @@
 (defun save-and-export ()
   "This is just a helper function."
   (interactive)
-        (save-buffer)
-        (org-latex-export-to-pdf))
+  (save-buffer)
+  (org-latex-export-to-pdf))
 
 (map! :mode 'org :leader "R" #'save-and-export)
 
@@ -192,9 +192,9 @@
   "Insert IEEE Org-mode LaTeX export template."
   (interactive)
   (insert "#+TITLE: insert title here\n")
-  (insert "#+SETUPFILE: /home/praaneshnair/.config/doom/ieee.org\n")
-  (insert "#+LATEX_HEADER: \\input{/home/praaneshnair/.config/doom/def.tex}\n")
-  (insert "#+LATEX_HEADER: \\input{/home/praaneshnair/.config/doom/author.tex}\n")
+  (insert "#+SETUPFILE: /home/praanesh-nair/.config/doom/ieee.org\n")
+  (insert "#+LATEX_HEADER: \\input{/home/praanesh-nair/.config/doom/def.tex}\n")
+  (insert "#+LATEX_HEADER: \\input{/home/praanesh-nair/.config/doom/author.tex}\n")
   (insert "#+LATEX_HEADER: \\graphicspath{ {.} }\n")
   (insert "#+OPTIONS: toc:nil ^:{} author:nil date:nil\n")
   (insert "#+BIBLIOGRAPHY: References.bib\n")
