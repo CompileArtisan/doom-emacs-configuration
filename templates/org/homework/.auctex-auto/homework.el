0c95e807fc6c48956590815ae429b176c65b2880
CompileArtisan <praanesh.b.nair@gmail.com>
2026-08-04 22:02:20 +0530

    snippet + neater hw template

---
+++ templates/org/homework/.auctex-auto/homework.el
@@ -0,0 +1,76 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "homework"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=1in") ("fontenc" "T1") ("xcolor" "usenames" "dvipsnames" "svgnames") ("tcolorbox" "many") ("mathtools" "") ("amsthm" "") ("amssymb" "") ("amsfonts" "") ("bm" "") ("thmtools" "") ("amsmath" "") ("marginnote" "") ("sidenotes" "") ("fancyhdr" "") ("titlesec" "") ("setspace" "") ("enumitem" "") ("graphicx" "") ("svg" "") ("hyperref" "bookmarks=true") ("bookmark" "") ("mathpazo" "") ("helvet" "scaled=0.92")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "geometry"
+    "fontenc"
+    "xcolor"
+    "tcolorbox"
+    "mathtools"
+    "amsthm"
+    "amssymb"
+    "amsfonts"
+    "bm"
+    "thmtools"
+    "amsmath"
+    "marginnote"
+    "sidenotes"
+    "fancyhdr"
+    "titlesec"
+    "setspace"
+    "enumitem"
+    "graphicx"
+    "svg"
+    "hyperref"
+    "bookmark"
+    "mathpazo"
+    "helvet")
+   (TeX-add-symbols
+    '("uppersum" ["argument"] 0)
+    '("lowersum" ["argument"] 0)
+    '("partition" ["argument"] 0)
+    '("func" ["argument"] 1)
+    '("cop" 1)
+    '("gfcb" 1)
+    '("hint" 1)
+    '("set" 1)
+    '("integral" 2)
+    '("absld" 1)
+    '("absle" 1)
+    '("abs" 1)
+    "eps"
+    "A"
+    "E"
+    "N"
+    "Q"
+    "R"
+    "Z"
+    "intab"
+    "fabr"
+    "fog"
+    "gof"
+    "deltaxi"
+    "deltaxk"
+    "Title"
+    "Author"
+    "Date"
+    "resetpar")
+   (LaTeX-add-xcolor-definecolors
+    "accent"
+    "accentlight"
+    "inkgray")
+   (LaTeX-add-tcolorbox-newtcolorboxes
+    '("answer" "" "" "")))
+ :latex)
+
