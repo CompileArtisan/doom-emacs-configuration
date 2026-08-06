0dc71ae0e3fcf00aa1f14c126bed0ae121983f70
CompileArtisan <praanesh.b.nair@gmail.com>
2026-08-06 23:28:00 +0530

    added source code formatting

--- templates/org/homework/.auctex-auto/homework.el
+++ templates/org/homework/.auctex-auto/homework.el
@@ -4,19 +4,21 @@
  "homework"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "margin=1in") ("fontenc" "T1") ("xcolor" "usenames" "dvipsnames" "svgnames") ("tcolorbox" "many") ("mathtools" "") ("amsthm" "") ("amssymb" "") ("amsfonts" "") ("bm" "") ("thmtools" "") ("amsmath" "") ("marginnote" "") ("sidenotes" "") ("fancyhdr" "") ("titlesec" "") ("setspace" "") ("enumitem" "") ("graphicx" "") ("svg" "") ("hyperref" "bookmarks=true") ("bookmark" "") ("mathpazo" "") ("helvet" "scaled=0.92")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("geometry" "margin=1in") ("fontenc" "T1") ("xcolor" "usenames" "dvipsnames" "svgnames") ("tcolorbox" "many") ("listings" "") ("etoolbox" "") ("mathtools" "") ("amsthm" "") ("amssymb" "") ("amsfonts" "") ("bm" "") ("thmtools" "") ("amsmath" "") ("marginnote" "") ("sidenotes" "") ("fancyhdr" "") ("titlesec" "") ("setspace" "") ("enumitem" "") ("graphicx" "") ("svg" "") ("hyperref" "bookmarks=true") ("bookmark" "") ("mathpazo" "") ("helvet" "scaled=0.92")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "geometry"
     "fontenc"
     "xcolor"
     "tcolorbox"
+    "listings"
+    "etoolbox"
     "mathtools"
     "amsthm"
     "amssymb"
@@ -69,8 +71,16 @@
    (LaTeX-add-xcolor-definecolors
     "accent"
     "accentlight"
-    "inkgray")
+    "inkgray"
+    "codebg"
+    "codekeyword"
+    "codecomment"
+    "codestring"
+    "codenumber"
+    "coderule")
    (LaTeX-add-tcolorbox-newtcolorboxes
-    '("answer" "" "" "")))
+    '("answer" "" "" ""))
+   (LaTeX-add-tcolorbox-tcbuselibraries
+    "listings,skins,breakable"))
  :latex)
 
